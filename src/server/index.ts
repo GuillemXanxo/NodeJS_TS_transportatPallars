03fb2222eaf328cf229ec102943ee55d594c2bc3
GuillemXanxo <jscodebaker@gmail.com>
2022-04-12 12:48:15 +0200

    Fixed imports in server/index

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,8 @@
 import express, { Application } from "express";
-import chalk from "chalk";
 import morgan from "morgan";
 import cors from "cors";
 import helmet from "helmet";
-import Debug from "debug";
 
-const debug = Debug("pallars: indexServer");
 const app: Application = express();
 
 app.use(cors());
@@ -13,4 +10,4 @@ app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 
-module.exports = { app };
+export default { app };
