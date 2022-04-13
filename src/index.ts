b9723f3ab043fa93569010bd94ff293b45cedd37
GuillemXanxo <jscodebaker@gmail.com>
2022-04-13 18:29:43 +0200

    Fixed environment data to be public

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { port, dataBaseString } from "./utils/environmentVariables";
 import initializeServer from "./server/initializeServer";
 import connectDB from "./dataBase";
 
 const { app } = require("./server");
 
+const port: number | string =
+  process.env.PORT ?? process.env.LOCAL_PORT ?? 5000;
+
 (async () => {
   try {
-    await connectDB(dataBaseString);
+    await connectDB(process.env.MONGODB_STRING);
     await initializeServer(port, app);
   } catch (error) {
     process.exit(1);
